95c844d1226b986d347c7dc9f97c71474dc06f14
Amanda Gray <floridabelle2021@protonmail.com>
2023-04-02 01:14:50 -0600

    set up questions array

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+const questions = [
+    {
+        "id": 0,
+        "question" : "Question 1",
+        "answers": ["Answer - 1", "Answer - 2", "Answer - 3", "Answer - 4"],
+        "right_answer": "Answer - 1"
+    },
+    {
+        "id": 2,
+        "question" : "Question 2",
+        "answers": ["Answer - 1", "Answer - 2", "Answer - 3", "Answer - 4"],
+        "right_answer": "Answer - 2"
+    },
+    {
+        "id": 3,
+        "question" : "Question 3",
+        "answers": ["Answer - 1", "Answer - 2", "Answer - 3", "Answer - 4"],
+        "right_answer": "Answer - 3"
+    },
+    {
+        "id": 4,
+        "question" : "Question 4",
+        "answers": ["Answer - 1", "Answer - 2", "Answer - 3", "Answer - 4"],
+        "right_answer": "Answer - 4"
+    },
+]
+
