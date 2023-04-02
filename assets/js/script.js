ae84a83352a9d564ffb388cdc709b88d30fa2098
Amanda Gray <floridabelle2021@protonmail.com>
2023-04-02 04:19:26 -0600

    worked on question checking logic and adding score

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,24 @@
-const questions = [
-    {
+const questions = [{
         "id": 0,
-        "question" : "Question 1",
+        "question": "Question 1",
         "answers": ["Answer - 1", "Answer - 2", "Answer - 3", "Answer - 4"],
         "right_answer": "Answer - 1"
     },
     {
-        "id": 2,
-        "question" : "Question 2",
+        "id": 1,
+        "question": "Question 2",
         "answers": ["Answer - 1", "Answer - 2", "Answer - 3", "Answer - 4"],
         "right_answer": "Answer - 2"
     },
     {
-        "id": 3,
-        "question" : "Question 3",
-        "answers": ["Answer - 1", "Answer - 2", "Answer - 3", "Answer - 4"],
+        "id": 2,
+        "question": "Question 3",
+        "answers": ["Answer - 1", "Answer - 2", "Answer - 3"],
         "right_answer": "Answer - 3"
     },
     {
-        "id": 4,
-        "question" : "Question 4",
+        "id": 3,
+        "question": "Question 4",
         "answers": ["Answer - 1", "Answer - 2", "Answer - 3", "Answer - 4"],
         "right_answer": "Answer - 4"
     },
@@ -30,9 +29,64 @@ let quizContainer = document.getElementById("quiz-container");
 let scoreContainer = document.getElementById("score-container");
 let highContainer = document.getElementById("high-score-container");
 let startButton = document.getElementById("start-button");
+let questionOption;
+let questionNumber = 0;
+let quizStart;
+let score = 0;
+
+
+function nextQuestion(questionNumber) {
+    quizStart = false;
+    console.log("questNum", questionNumber)
+    let questionCard = document.querySelectorAll(".container_mid");
+    questionCard.forEach((card) => {
+        card.classList.add("hide");
+    });
+    questionCard[questionNumber].classList.remove("hide");
+}
+
+function checkAnswer(answerClicked) {
+   answerClicked = answerClicked.innerText
+   if (answerClicked == questions[questionNumber].right_answer) {
+    score++
+   }
+    questionNumber += 1;
+    if (questionNumber == questions.length) {
+        quizContainer.classList.add("hide")
+        scoreContainer.classList.remove("hide")
+    } else {
+        nextQuestion(questionNumber)
+    }
+    console.log("score", score)
+}
+
+function startQuiz() {
+    for (let i of questions) {
+        let questionCard = document.createElement("div");
+        questionCard.classList.add("container_mid");
+
+        let question_DIV = document.createElement("p");
+        question_DIV.classList.add("question");
+        question_DIV.innerHTML = i.question;
+
+        questionCard.appendChild(question_DIV);
+
+        console.log(i.answers)
+        for (let x = 0; x < i.answers.length; x++) {
+            console.log(x)
+            questionCard.innerHTML += `
+            <button id="question-option" class="option-div" onclick="checkAnswer(this)">${i.answers[x]}</button>
+            `;
+        }
+        quizContainer.appendChild(questionCard);
+    }
+}
 
 startButton.addEventListener("click", () => {
-	startContainer.classList.add("hide")
+    startContainer.classList.add("hide")
     quizContainer.classList.remove("hide")
+    quizStart = true
     // start timer function
+    startQuiz();
+    nextQuestion(0);
 });
